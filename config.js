5dc3e7fbaf7073825cf06064395f868e57313abf
angriogon <105793447+angriogon@users.noreply.github.com>
2026-09-12 18:36:14 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+window.OPI_CONFIG = {
+  googleClientId: '',
+  firebase: {
+    apiKey: "AIzaSyC2ZwO_mmlcMy546aM6Rsw5fBGdJhBfPE4",
+    authDomain: "organizador-dacc3.firebaseapp.com",
+    projectId: "organizador-dacc3",
+    storageBucket: "organizador-dacc3.firebasestorage.app",
+    messagingSenderId: "589165533669",
+    appId: "1:589165533669:web:57fe61029bdf5f5b63f70c",
+    measurementId: "G-THCFYXQW0Y"
+  }
+};
